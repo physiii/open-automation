b07bc8dad07d8a86ee848417e0aac7023ba4821d
physiii <andy@pyfi.org>
2016-02-06 18:01:14 +0000

    ip search

--- gateway.js
+++ gateway.js
@@ -65,6 +65,32 @@ for(var i = 0; i < device_info.length; i++) {
     io_relay.emit('token',{token:token});    
     console.log(mac + " | " + token);
 }
+
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+    ++alias;
+  });
+});
+
+
 /// create table if it does not exist ///
 /*
 pool.getConnection(function(err, connection) {
