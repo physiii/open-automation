c15a7a6953ed8797d3b18aebf6a7f2f86375ae53
Andy Payne <andy@pyfi.org>
2016-03-07 00:16:39 +0000

    update

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 sudo apt-get install apache2 mysql-server php5 php5-mysql php5-gd motion libmysqlclient-dev libcurl4-openssl-dev
 sudo cp motion.conf /etc/motion/motion.conf
 sudo cp motion /etc/default/motion
-mkdir ~/audio ~/video ~/documents
+sudo cp rc.local /etc/rc.local
 sudo chmod -R 777 /var/lib/motion
-ln -s ~/audio ./files/
-ln -s ~/video ./files/
-ln -s ~/documents ./files/
+mkdir ~/Audio ~/Videos ~/Documents /var/lib/motion/video /var/lib/motion/images
+ln -s /var/lib/motion files
+ln -s ~/Audio files/
+ln -s ~/Videos files/
+ln -s ~/Documents files/
