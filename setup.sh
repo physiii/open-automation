3e8aba7d2f14efd0898e84cdbaf09771efa372c7
physiii <andy@pyfi.org>
2016-04-25 07:56:32 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+sudo apt-get update
 sudo apt-get install -y xdotool apache2 mysql-server php5 php5-mysql php5-gd motion libmysqlclient-dev libcurl4-openssl-dev
 sudo cp motion.conf /etc/motion/motion.conf
 sudo cp motion /etc/default/motion
