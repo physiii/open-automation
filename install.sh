7068452a2d4a49076ca6d3a0cd576ac7fae38284
physiii <andy@pyfi.org>
2016-05-29 10:23:52 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt-get install -y libjpeg-dev libavformat56 libavformat-dev libavcodec56 libavcodec-dev libavutil54 libavutil-dev libc6-dev zlib1g-dev libmysqlclient18 libmysqlclient-dev libpq5 libpq-dev
-sudo apt-get install -y tmux xdotool apache2 mysql-server libapache2-mod-php php php-mysql php-gd motion libmysqlclient-dev libcurl4-openssl-dev
+sudo apt-get install -y tmux xdotool apache2 mysql-server php5 php5-mysql php5-gd motion libmysqlclient-dev libcurl4-openssl-dev
+#sudo apt-get install -y tmux xdotool apache2 mysql-server libapache2-mod-php php php-mysql php-gd motion libmysqlclient-dev libcurl4-openssl-dev
 sudo cp motion.conf /etc/motion/motion.conf
 sudo cp motion /etc/default/motion
 sudo cp rc.local /etc/rc.local
