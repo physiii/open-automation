5004f57ba9c88d9a9ed0ca5e48aac3baa58af758
physiii <andy@pyfi.org>
2016-07-12 22:18:12 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 sudo apt-get update
-sudo apt-get install -y mongodb dnsmasq hostapd pkg-config libudev-dev libjpeg-dev libavformat56 libavformat-dev libavcodec56 libavcodec-dev libavutil54 libavutil-dev libc6-dev zlib1g-dev libmysqlclient18 libmysqlclient-dev libpq5 libpq-dev
-sudo apt-get install -y tmux xdotool apache2 mysql-server php5 php5-mysql php5-gd motion libmysqlclient-dev libcurl4-openssl-dev
+sudo apt-get install -y mongodb dnsmasq hostapd pkg-config libudev-dev libjpeg-dev libavformat56 libavformat-dev libavcodec56 libavcodec-dev libavutil54 libavutil-dev libc6-dev zlib1g-dev libmysqlclient18 libmysqlclient-dev libpq5 libpq-dev tmux xdotool apache2 mysql-server php5 php5-mysql php5-gd motion libmysqlclient-dev libcurl4-openssl-dev
+wget https://github.com/ekarak/openzwave-debs-raspbian/raw/master/v1.4.1/libopenzwave1.3-dev_1.3.532.ge3defea_armhf.deb
+sudo dpkg -i libopenzwave*.deb
 #sudo apt-get install -y tmux xdotool apache2 mysql-server libapache2-mod-php php php-mysql php-gd motion libmysqlclient-dev libcurl4-openssl-dev
 #sudo cp motion.conf /etc/motion/motion.conf
 #sudo cp motion /etc/default/motion
-sudo cp rc.local /etc/rc.local
+#sudo cp rc.local /etc/rc.local
 sudo chmod -R 777 /var
 rm files/Audio files/Videos files/Documents files/motion
 #mkdir ~/Audio ~/Videos ~/Documents /var/lib/motion/video /var/lib/motion/images
