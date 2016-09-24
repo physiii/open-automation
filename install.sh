23f990f68d859ea854b7b0caf84e758ded5c39b3
physiii <andy@pyfi.org>
2016-09-24 13:51:38 +0000

    update

--- install.sh
+++ install.sh
@@ -21,3 +21,17 @@ ln -s ~/Audio files/
 ln -s ~/Videos files/
 ln -s ~/Documents files/
 echo "enable raspicam!"
+
+
+##ffmpeg
+git clone git://git.videolan.org/x264
+cd x264
+./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl
+make
+sudo make install
+cd ..
+git clone https://github.com/FFmpeg/FFmpeg.git
+cd FFmpeg
+sudo ./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree
+make
+sudo make install
