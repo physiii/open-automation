98e27128a6e93739b2d3ec540cce7a2d5ad1f5da
physiii <andy@pyfi.org>
2017-03-07 19:04:46 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/sh -e
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get update
-sudo apt-get install -y speedtest-cli gstreamer1.0 v4l2loopback-dkms v4l2loopback-utils git nodejs mongodb dnsmasq hostapd pkg-config libudev-dev libjpeg-dev libavformat-dev libavcodec-dev libavutil-dev libc6-dev zlib1g-dev libmysqlclient-dev libpq5 libpq-dev tmux xdotool apache2 mysql-server libmysqlclient-dev libcurl4-openssl-dev
+sudo apt-get install -y npm motion speedtest-cli gstreamer1.0 v4l2loopback-dkms v4l2loopback-utils git nodejs mongodb dnsmasq hostapd pkg-config libudev-dev libjpeg-dev libavformat-dev libavcodec-dev libavutil-dev libc6-dev zlib1g-dev libmysqlclient-dev libpq5 libpq-dev tmux xdotool apache2 mysql-server libmysqlclient-dev libcurl4-openssl-dev
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo chmod -R 777 /var
 sudo chmod -R 777 /usr/src
 
-rm files/Audio files/Videos files/Documents files/motion
+#rm files/Audio files/Videos files/Documents files/motion
 #mkdir ~/Audio ~/Videos ~/Documents /var/lib/motion/video /var/lib/motion/images
-mkdir ~/Audio ~/Videos ~/Documents files
-ln -s /var/lib/motion files
-ln -s ~/Audio files/
-ln -s ~/Videos files/
-ln -s ~/Documents files/
+#mkdir ~/Audio ~/Videos ~/Documents files
+#ln -s /var/lib/motion files
+#ln -s ~/Audio files/
+#ln -s ~/Videos files/
+#ln -s ~/Documents files/
 echo "enable raspicam!"
 
 #TODO: just include the binary
@@ -48,3 +48,7 @@ cd FFmpeg
 sudo ./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree
 make
 sudo make install
+
+##install open-automation
+sudo npm install -g pm2
+npm install
