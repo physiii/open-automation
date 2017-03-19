d84e61d23c6bcc1f10ccf21ab27dc067f38354c2
physiii <andy@pyfi.org>
2017-03-19 07:50:17 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get update
-sudo apt-get install -y bc libudev-dev nmap npm motion speedtest-cli gstreamer1.0 v4l2loopback-dkms v4l2loopback-utils git nodejs npm mongodb dnsmasq hostapd pkg-config libudev-dev libjpeg-dev libavformat-dev libavcodec-dev libavutil-dev libc6-dev zlib1g-dev libpq5 libpq-dev tmux xdotool
+sudo apt-get install -y bc libudev-dev nmap motion speedtest-cli gstreamer1.0 v4l2loopback-dkms v4l2loopback-utils git nodejs npm mongodb dnsmasq hostapd pkg-config libudev-dev libjpeg-dev libavformat-dev libavcodec-dev libavutil-dev libc6-dev zlib1g-dev libpq5 libpq-dev tmux xdotool
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo chmod -R 777 /var/log /var/lib /usr/src
 sudo chmod a+rwxt /var/run/motion
