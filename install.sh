287dfcb6767a65b03091748789125e4c1ecf498a
physiii <andy@pyfi.org>
2017-04-10 14:36:27 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ sudo apt-get update
 #sudo apt-get install -y g++ lua5.2 raspberrypi-kernel-headers bc sshpass libudev-dev nmap motion speedtest-cli gstreamer1.0 v4l2loopback-dkms v4l2loopback-utils git nodejs mongodb dnsmasq hostapd pkg-config libudev-dev libjpeg-dev libavformat-dev libavcodec-dev libavutil-dev libc6-dev zlib1g-dev libpq5 libpq-dev tmux xdotool
 sudo apt-get install -y bc g++ lua5.2 bc sshpass libudev-dev nmap motion speedtest-cli gstreamer1.0 v4l2loopback-dkms v4l2loopback-utils git nodejs mongodb dnsmasq hostapd pkg-config libudev-dev libjpeg-dev libavformat-dev libavcodec-dev libavutil-dev libc6-dev zlib1g-dev libpq5 libpq-dev tmux xdotool
 sudo ln -s /usr/bin/nodejs /usr/bin/node
-sudo chmod -R 777 /var/log /var/lib /usr/src
 sudo chmod a+rwxt /var/run/motion
 sudo cp files/motion.conf /etc/motion/motion.conf
 sudo cp files/default.motion /etc/default/motion
 sudo service motion restart
+sudo chmod -R 777 /var/log /var/lib /usr/src
 
-#TODO: just include the binary
+## make/install openzwave
 cd /usr/src
 wget http://old.openzwave.com/downloads/openzwave-1.4.1.tar.gz
 tar zxvf openzwave-1.4.1.tar.gz
