1563a9cd8311f4fa88798c65d9c239361e7010b8
Your Name <you@example.com>
2017-04-17 15:22:08 -0500

    update

---
+++ relay/public/js/controllers/system_generator.js
@@ -0,0 +1,6 @@
+angular.module('starter.controllers')
+
+.controller('SystemGeneratorCtrl', function($scope, $rootScope, $stateParams, Categories, socket, $ionicLoading, $compile, $http) {
+  console.log('<< ------  SystemGeneratorCtrl  ------ >>');
+
+})
