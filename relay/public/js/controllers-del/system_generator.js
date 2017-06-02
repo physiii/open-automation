aa9d9b74ed1ec11fe28da3efad9dc5f9392f3f6c
Your Name <you@example.com>
2017-06-02 08:21:09 -0500

    update

--- relay/public/js/controllers-del/system_generator.js
+++
@@ -1,6 +0,0 @@
-angular.module('starter.controllers')
-
-.controller('SystemGeneratorCtrl', function($scope, $rootScope, $stateParams, Categories, socket, $ionicLoading, $compile, $http) {
-  console.log('<< ------  SystemGeneratorCtrl  ------ >>');
-
-})
