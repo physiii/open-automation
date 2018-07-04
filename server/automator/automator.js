f8c6c513f20d0fe9c4242920b0c06a275e45c475
JPollock89 <jeremy-pollock@raider.rose.edu>
2018-07-04 11:41:55 -0500

    WIP: building functions for database loading and add/create devices in automator.

--- server/automator/automator.js
+++ server/automator/automator.js
@@ -1,18 +1,36 @@
 const database = require('../database.js'),
 	Automation = require('./automation.js'),
-	automationList = new Map(),
+	automationsList = new Map(),
 	TAG = '[Automator.js]';
 
 class Automator {
 	constructor () {
-
 	}
 
-	addAutomation (data) {}
+	addAutomation (data) {
+		let automation = this.getAutomationById(data.id,null,true);
 
-	createAutomation (data) {}
+		if (automation) {
+			return automation;
+		}
 
-	handleAutomationUpdate () {}
+		automation = new Automation(data);
+
+		automationsList.set(automation.id, automation);
+	}
+
+	createAutomation (data) {
+		return new Promise((resolve, reject) => {
+			const automation = this.addAutomation(data);
+
+//TODO: Need Database functionality for creating automations
+
+			database.saveAutomation(automation.dbSerialize()).then(() => {
+				resolve(automation);
+			}).catch(reject);
+		});
+	}
+
 
 	triggerAutomation (data) {
 		switch (data.type) {
@@ -29,7 +47,31 @@ class Automator {
 		}
 	}
 
-	loadAutomationsFromDb () {}
+	getAutomationById (automationId, accountId, skipAccountAccessCheck) {
+		const automation = automationsList.get(automationId);
+
+		//Verify account has the access to the automations
+		if ((automation && (automation.location === accountId)) || skipAccountAccessCheck) {
+			return automation;
+		}
+
+	}
+
+	loadAutomationsFromDb () {
+		return new Promise((resolve, reject) => {
+			database.getAutomations().then((automations) => {
+				automationsList.clear();
+
+				automations.forEach((automation) => {
+					this.addAutomation(automation)
+				});
+
+				resolve(automationsList);
+			}).catch((error) => {
+				reject(error);
+			});
+		});
+	}
 }
 
 module.exports = new Automation();
