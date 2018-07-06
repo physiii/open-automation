2a1ba1c31b7dd2c490ffccabd972d3e56d9a0271
Jeremy Pollock <jeremy-pollock@raider.rose.edu>
2018-07-06 12:29:14 -0500

    WIP: add basic scene creation class

--- server/scenes/scene.js
+++ server/scenes/scene.js
@@ -1,1 +1,27 @@
-//placehold scene class
+const uuid = require('uuid/v4'),
+	database = require('../database.js'),
+	TAG = '[Scene.js]';
+
+class Scene {
+	constructor (data) {
+		this.id = data.id || uuid();
+		this.type = data.type || ''
+		this.actions = data.actions || [];
+	}
+
+	serialize () {
+		return {
+			id: this.id,
+			type: this.type,
+			actions: this.actions
+		};
+	}
+
+	dbSerialize () {
+		return {
+			...this.serialize()
+		};
+	}
+}
+
+module.exports = Scene;
