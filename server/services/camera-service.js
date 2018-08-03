3b31d347854ff61db8f6af55493b169a3c3de37f
Jeremy Pollock <jeremy-pollock@raider.rose.edu>
2018-08-03 11:23:16 -0500

    added needed + sign to code

--- server/services/camera-service.js
+++ server/services/camera-service.js
@@ -80,7 +80,7 @@ CameraService.event_strings = {
 		getHtmlDescription: function (event_data, attachment) {
 			return (
 				'<p>' + this.getNameOrType(true, true, true) + ' recorded movement at ' + moment(event_data.recording.date).format('h:mm a on dddd, MMMM Do.') + '</p>'
-				'<p>' +
+				+ '<p>' +
 				'<a href="' + this.getRecordingLink(event_data.recording.id, event_data.recording.date) + '">' +
 				'Play Recording<br />' +
 				(attachment ? '<img src="cid:' + attachment.cid + '" />' : '') +
