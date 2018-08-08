242d20170edd1d6b6e513c7a9b28457db2f697ab
Jeremy Pollock <jeremy-pollock@raider.rose.edu>
2018-08-08 09:25:41 -0500

    Added driver export

--- server/services/drivers/button-generic.js
+++ server/services/drivers/button-generic.js
@@ -1,9 +1,11 @@
 const GatewayServiceDriver = require('./gateway.js');
 
-class GernericButtonDriver extends GatewayServiceDriver {
+class GenericButtonDriver extends GatewayServiceDriver {
 	constructor (button_id, gateway_socket) {
 		super(button_id, 'button', gateway_socket);
 	}
 
 	listenToSocket () {}
 }
+
+module.exports = GenericButtonDriver;
