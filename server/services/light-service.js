c6ef7b855cb482a9ffa2830ffe4223bd9ad97904
Ethan Sisson <ethan@ethansisson.com>
2018-08-14 09:26:04 -0500

    Added framework for service actions

--- server/services/light-service.js
+++ server/services/light-service.js
@@ -2,45 +2,9 @@ const Service = require('./service.js'),
 	TAG = '[LightService]';
 
 class LightService extends Service {
-	action (data) {
-		switch (data.property) {
-			case 'power':
-				if (data.toggle)	return this.state.power ? this.lightOff() : this.lightOn();
-
-				if (data.value === true) {
-					return this.lightOn();
-				} else if (data.value === false) {
-					return this.lightOff();
-				} else {
-					return console.log(TAG, 'action value invalid. Must be true or false');
-				}
-			case 'brightness':
-				if (!data.mode && !(data.value < 0) && !(data.value > 100)) return this.setBrightness(data.value);
-				if (data.mode === 'add' && this.state.brightness >= 100) return this.state.brightness = 100;
-				if (data.mode === 'add') return this.setBrightness(this.state.brightness + data.value);
-				if (data.mode === 'subtract' && this.state.brightness <= 0) return this.state.brightness = 0;
-				if (data.mode === 'subtract') return this.setBrightness(this.state.brightness - data.value);
-			case 'color':
-				return this.setColor(data.value);
-		}
-	}
-
-	lightOn () {
+	setPower (power) {
 		return new Promise((resolve, reject) => {
-			this.deviceEmit('lightOn/set', {}, (error, data) => {
-				if (error) {
-					reject(error);
-					return;
-				}
-
-				resolve();
-			});
-		});
-	}
-
-	lightOff () {
-		return new Promise((resolve, reject) => {
-			this.deviceEmit('lightOff/set', {}, (error, data) => {
+			this.deviceEmit(power ? 'lightOn/set' : 'lightOff/set', {}, (error, data) => {
 				if (error) {
 					reject(error);
 					return;
@@ -81,5 +45,19 @@ class LightService extends Service {
 LightService.type = 'light';
 LightService.friendly_type = 'Light';
 LightService.indefinite_article = 'A';
+LightService.state_definitions = {
+	power: {
+		type: 'boolean',
+		setter: 'setPower'
+	},
+	brightness: {
+		type: 'percentage',
+		setter: 'setBrightness'
+	},
+	color: {
+		type: 'color',
+		setter: 'setColor'
+	}
+};
 
 module.exports = LightService;
