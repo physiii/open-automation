9b8bc79f43422dfe43549f7f7eda82a4af3d1967
Jeremy Pollock <jeremy-pollock@raider.rose.edu>
2018-08-31 16:09:45 -0500

    Sending latest code

--- server/services/siren-service.js
+++ server/services/siren-service.js
@@ -1,10 +1,26 @@
 const Service = require('./service.js'),
 	TAG = '[SirenService]';
 
-class SirenService extends Service {}
+class SirenService extends Service {
+	setSiren (trigger) {
+		return new Promise((resolve, reject) => {
+			if (trigger) {
+				this.deviceEmit('siren', {chirp:2});
+			}
+
+			resolve();
+		});
+	}
+}
 
 SirenService.type = 'siren';
 SirenService.friendly_type = 'Siren';
 SirenService.indefinite_article = 'A';
+SirenService.state_definitions = {
+	trigger: {
+		type: 'boolean',
+		setter: 'setSiren'
+	}
+};
 
 module.exports = SirenService;
