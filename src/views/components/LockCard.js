4837439b0db5137176e691fb824b95adc7b470e4
Jeremy Pollock <jeremy-pollock@raider.rose.edu>
2018-09-19 15:37:07 -0500

    update lockCard

--- src/views/components/LockCard.js
+++ src/views/components/LockCard.js
@@ -20,7 +20,6 @@ export const LockCard = (props) => {
 			name={props.service.settings.name || 'Lock'}
 			status={props.service.state.locked ? 'Locked' : 'Unlocked'}
 			isConnected={props.service.state.connected}
-			onCardClick={toggleLock}
 			{...props}>
 			<Button onClick={toggleLock}>{props.service.state.locked ? 'Unlock' : 'Lock'}</Button>
 		</ServiceCardBase>
