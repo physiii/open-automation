a8c7b6ba200943bb354198310d62b2b7ee70b949
Ethan Sisson <ethan@ethansisson.com>
2018-11-13 13:20:29 -0600

    Additional logging around closing device sockets

--- server/devices/drivers/device-driver.js
+++ server/devices/drivers/device-driver.js
@@ -1,3 +1,5 @@
+const TAG = '[DeviceDriver]';
+
 class DeviceDriver {
 	constructor (data, socket, device_id) {
 		this.device_id = device_id;
@@ -22,6 +24,7 @@ class DeviceDriver {
 
 		// Disconnect the current socket.
 		if (this.socket) {
+			console.log(TAG, 'Closing device socket to set new socket.', this.device_id);
 			this.socket.removeAllListeners();
 			this.socket.disconnect();
 		}
