6bee49e115e9d4d041bd27d65dfd894bb8041a03
Andy Payne <andy@pyfi.org>
2020-02-12 11:21:06 -0600

    BUG FIX: moment library need to be required

--- server/services/contact-sensor-service.js
+++ server/services/contact-sensor-service.js
@@ -1,4 +1,5 @@
-const Service = require('./service.js');
+const moment = require('moment'),
+	Service = require('./service.js');
 
 class ContactSensorService extends Service {
 	subscribeToDevice () {
@@ -12,5 +13,34 @@ class ContactSensorService extends Service {
 ContactSensorService.type = 'contact-sensor';
 ContactSensorService.friendly_type = 'Contact Sensor';
 ContactSensorService.indefinite_article = 'A';
+
+ContactSensorService.event_definitions = new Map([...Service.event_definitions])
+	.set('open', {
+		label: 'Machine Opened',
+		generateNotification: function (event_data) {
+			return 'Movement was detected on ' + this.getNameOrType(true, false, true) + ' opened at ' + moment(event_data.date).format('h:mm a on dddd, MMMM Do.');
+		}
+	})
+	.set('closed', {
+		label: 'Machine Closed',
+		generateNotification: function (event_data) {
+			return 'Movement on ' + this.getNameOrType(true, false, true) + ' closed at ' + moment(event_data.date).format('h:mm a on dddd, MMMM Do.');
+		}
+	});
+
+ContactSensorService.event_strings = {
+	'open': {
+		getFriendlyName: () => 'Opened',
+		getDescription: function (event_data) {
+			return 'Movement was detected on ' + this.getNameOrType(true, false, true) + ' at ' + moment(event_data.date).format('h:mm a on dddd, MMMM Do.');
+		}
+	},
+	'closed': {
+		getFriendlyName: () => 'Closed',
+		getDescription: function (event_data) {
+			return 'Movement on ' + this.getNameOrType(true, false, true) + ' stopped at ' + moment(event_data.date).format('h:mm a on dddd, MMMM Do.');
+		}
+	}
+};
 
 module.exports = ContactSensorService;
